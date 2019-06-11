95f5edbdd4a426356b4bb204aa12de85178e272c
Francisco Dueñas <frsduenasca@unal.edu.co>
2019-06-11 17:58:19 -0500

    Se agrego el package "Entidad" con las clases "Restaurante" y "Asignacion"

---
+++ src/Entidad/Asignacion.java
@@ -0,0 +1,47 @@
+package Entidad;
+
+public class Asignacion {
+
+    private String restaurante;
+    private String persona;
+    private String turno;
+    private String mesa;
+
+    public Asignacion() {
+    }
+
+    public String getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(String restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public String getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(String mesa) {
+        this.mesa = mesa;
+    }
+    
+    
+    
+}
---
+++ src/Entidad/Restaurante.java
@@ -0,0 +1,45 @@
+package Entidad;
+
+public class Restaurante {
+    
+    private String nombre;
+    private String ubicacion;
+    private String tipo;
+    private String horario;
+
+    public Restaurante() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    
+}
